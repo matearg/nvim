bb043d0b13cb7a8546241373f08ff3da395401be
matearg <mateoarcidiacono@gmail.com>
2022-03-18 00:57:27 -0300

    Lsp settings updated

--- lua/plugins/lsp/handlers.lua
+++ lua/plugins/lsp/handlers.lua
@@ -72,7 +72,7 @@ end
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { "clangd", "cssls", "eslint", "html", "jsonls", "pyright", "rome" }
+local servers = { "cssls", "eslint", "html", "jsonls", "pyright", "rome" }
 for _, lsp in pairs(servers) do
   require('lspconfig')[lsp].setup {
     on_attach = on_attach,
