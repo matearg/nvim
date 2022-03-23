39f9e92eeb9ac9f1ea35988faaedf3d9b157cd69
matearg <mateoarcidiacono@gmail.com>
2022-03-23 02:14:29 -0300

    Changed colorscheme options

--- lua/colors/color-configs.lua
+++ lua/colors/color-configs.lua
@@ -3,7 +3,10 @@ vim.g.gruvbox_material_enable_italic = 0
 vim.g.gruvbox_material_disable_italic_comment = 1
 vim.g.gruvbox_material_background = 'medium'  -- Avalible 'soft', 'medium', 'hard'
 vim.g.gruvbox_material_enable_bold = 1
-vim.g.gruvbox_material_transparent_background = 0
+vim.g.gruvbox_material_transparent_background = 1
+vim.g.gruvbox_material_palette = 'original'
+vim.g.gruvbox_material_statusline_style = 'original'
+vim.g.gruvbox_material_visual = 'reverse'
 -- Tokyonight
 vim.g.tokyonight_disable_italic_comment = 1
 vim.g.tokyonight_enable_italic = 0
