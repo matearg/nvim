f934b96b04d14215ff983c5693d688c588867eac
matearg <mateoarcidiacono@gmail.com>
2022-03-23 19:52:10 -0300

    Minnor changes

--- lua/colors/color-configs.lua
+++ lua/colors/color-configs.lua
@@ -24,7 +24,7 @@ vim.g.dracula_colorterm = 1
 vim.g.vscode_style = "dark"
 vim.g.vscode_transparent = true
 vim.g.vscode_italic_comment = 0
-vim.g.vscode_disable_nvimtree_bg = false
+vim.g.vscode_disable_nvimtree_bg = true
 -- OneDark
 require('onedark').setup  {
     style = 'dark', -- Chose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light'
