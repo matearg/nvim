7a593be5c9cd77b30f8fcf2d02aaff628e9ae2be
matearg <mateoarcidiacono@gmail.com>
2022-06-16 19:13:37 -0300

    Changed some colorschemes

--- lua/colors/color-configs.lua
+++ lua/colors/color-configs.lua
@@ -10,12 +10,6 @@ vim.g.gruvbox_material_statusline_style = 'original' -- Avalible 'material', 'or
 vim.g.gruvbox_material_visual = 'reverse'
 vim.g.gruvbox_material_diagnostic_virtual_text = 'colored'
 
--- VScode.nvim
-vim.g.vscode_style = 'dark'
-vim.g.vscode_transparent = false
-vim.g.vscode_italic_comment = 0
-vim.g.vscode_disable_nvimtree_bg = true
-
 -- OneDark
 require('onedark').setup  {
     style = 'darker', -- Chose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light'
@@ -33,14 +27,3 @@ require('onedark').setup  {
         background = true,
     },
 }
-
--- Calvera dark
-vim.g.calvera_italic_comments = false
-vim.g.calvera_italic_keywords = false
-vim.g.calvera_italic_functions = false
-vim.g.calvera_italic_variables = false
-vim.g.calvera_contrast = true
-vim.g.calvera_borders = true
-vim.g.calvera_disable_background = false
-vim.g.transparent_bg = false
-vim.g.calvera_hide_eob = true
