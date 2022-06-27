5595317972a9b51b4e4024d025a986162cd23414
matearg <mateoarcidiacono@gmail.com>
2022-06-27 00:03:18 -0300

    Changed colors

--- lua/colors/color-configs.lua
+++ lua/colors/color-configs.lua
@@ -2,7 +2,7 @@
 vim.g.gruvbox_material_enable_italic = 0
 vim.g.gruvbox_material_disable_italic_comment = 1
 vim.g.gruvbox_material_background = 'medium'  -- Avalible 'soft', 'medium', 'hard'
-vim.g.gruvbox_material_enable_bold = 1
+vim.g.gruvbox_material_enable_bold = 0
 vim.g.gruvbox_material_transparent_background = 1
 vim.g.gruvbox_material_show_eob = 0
 vim.g.gruvbox_material_palette = 'original' -- Avalible 'material', 'original', 'mix'
@@ -13,7 +13,7 @@ vim.g.gruvbox_material_diagnostic_virtual_text = 'colored'
 -- OneDark
 require('onedark').setup  {
     style = 'darker', -- Chose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light'
-    transparent = true,
+    transparent = false,
     code_style = {
         comments = 'none',
         keywords = 'none',
