d688c5f1ac138694e67af7aaaf62f9735adc8150
matearg <mateoarcidiacono@gmail.com>
2022-06-27 16:27:21 -0300

    Added another option for lualine

--- lua/plugins/configs/lualine.lua
+++ lua/plugins/configs/lualine.lua
@@ -3,46 +3,56 @@ if not status_ok then
   return
 end
 
+local hide_in_width = function()
+  return vim.fn.winwidth(0) > 80
+end
+
+local diagnostics = {
+  "diagnostics",
+  sources = { "nvim_diagnostic" },
+  sections = { "error", "warn" },
+  symbols = { error = " ", warn = " " },
+  colored = false,
+  always_visible = true,
+}
+
+local diff = {
+  "diff",
+  colored = false,
+  symbols = { added = "", modified = "", removed = "" }, -- changes diff symbols
+  cond = hide_in_width,
+}
+
+local filetype = {
+  "filetype",
+  icons_enabled = true,
+}
+
+local location = {
+  "location",
+  padding = 0,
+}
+
+local spaces = function()
+  return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
+end
+
 lualine.setup {
   options = {
+    globalstatus = false,
     icons_enabled = true,
-    theme = 'auto',
-    component_separators = { left = '', right = ''}, -- Avalible '', { left = '', right = ''}, { left = '', right = ''}, { left = '', right = ''}
-    section_separators = { left = '', right = ''}, -- Avalible '', { left = '', right = ''}, { left = '', right = ''}, { left = '', right = ''}
-    globalstatus = true,
+    theme = "auto",
+    component_separators = { left = "", right = "" },
+    section_separators = { left = "", right = "" },
+    disabled_filetypes = { "alpha", "dashboard" },
+    always_divide_middle = true,
   },
   sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch',
-      {
-      'diff',
-      colored = true, -- Displays a colored diff status if set to true
-      diff_color = {
-        -- Same color values as the general color option can be used here.
-        -- added    = 'DiffAdd',    -- Changes the diff's added color
-        -- modified = 'DiffChange', -- Changes the diff's modified color
-        -- removed  = 'DiffDelete', -- Changes the diff's removed color you
-      },
-      symbols = {added = '+', modified = '~', removed = '-'}, -- Changes the symbols used by the diff.
-      source = nil, -- A function that works as a data source for diff.
-                    -- It must return a table as such:
-                    --   { added = add_count, modified = modified_count, removed = removed_count }
-                    -- or nil on failure. count <= 0 won't be displayed.
-      }, 'diagnostics'
-    },
-    lualine_c = {{'filename', path = 1}},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
+    lualine_a = { "mode" },
+    lualine_b = {"branch"},
+    lualine_c = { diagnostics },
+    lualine_x = { diff, spaces, "encoding", filetype },
+    lualine_y = { location },
+    lualine_z = { "progress" },
   },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
-    lualine_y = {},
-    lualine_z = {}
-  },
-  tabline = {},
-  extensions = {}
 }
