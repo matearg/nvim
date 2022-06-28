0d52020d77d705126733b0f7c7f08c8b688264b2
matearg <mateoarcidiacono@gmail.com>
2022-06-28 15:47:47 -0300

    Changed floaterm borders

--- lua/plugins/configs/floaterm.lua
+++ lua/plugins/configs/floaterm.lua
@@ -1,4 +1,4 @@
 -- Floaterm configs
 vim.g.floaterm_shell= '"C:\\Users\\mateo\\AppData\\Local\\Microsoft\\WindowsApps\\Microsoft.PowerShell_8wekyb3d8bbwe\\pwsh.exe" -nologo'
 vim.g.floaterm_title = "Terminal"
-vim.g.floaterm_borderchars = "─│─│┌┐┘└" -- Avalible "─│─│╭╮╯╰", "─│─│┌┐┘└", "═║═║╔╗╝╚"
+vim.g.floaterm_borderchars = "─│─│╭╮╯╰" -- Avalible "─│─│╭╮╯╰", "─│─│┌┐┘└", "═║═║╔╗╝╚"
