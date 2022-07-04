a41c940dcd63c5a3373240b7d5449ca7191c50a6
Mateo Arcidiacono <mateoarcidiacono@gmail.com>
2022-07-04 12:36:12 -0300

    Changed colorways

--- lua/colors/colorscheme.lua
+++ lua/colors/colorscheme.lua
@@ -1,6 +1,6 @@
 vim.cmd [[
 try
-  colorscheme tokyonight
+  colorscheme onenord
 catch /^Vim\%((\a\+)\)\=:E185/
   colorscheme default
 endtry
--- lua/plugins/configs/eviline.lua
+++ lua/plugins/configs/eviline.lua
@@ -6,10 +6,10 @@ local lualine = require('lualine')
 -- Color table for highlights
 -- stylua: ignore
 local colors = {
-  bg       = 'auto',                 -- for transparent
+  -- bg       = 'auto',                 -- for transparent
   -- bg       = '#222222',              -- For darkplus
   -- bg       = '#282c34',              -- For onedark
-  -- bg       = '#3b4252',              -- For onenord
+  bg       = '#3b4252',              -- For onenord
   -- bg       = '#232534',              -- for palenightfall
   -- bg       = '#1d202f',              -- for tokyonight
 
