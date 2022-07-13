7ee95218ecb17b2c506207b90695a5edf46f93a7
Mateo Arcidiacono <mateoarcidiacono@gmail.com>
2022-07-13 20:48:33 -0300

    Chaged some folders

--- lua/colors/color-configs/init.lua
+++
@@ -1,9 +0,0 @@
--- Put your colorscheme config files down below
-require "colors.color-configs.settings.catppuccin"
-require "colors.color-configs.settings.dracula"
-require "colors.color-configs.settings.gruvbaby"
-require "colors.color-configs.settings.kanagawa"
-require "colors.color-configs.settings.nord"
--- require "colors.color-configs.settings.onedark"
--- require "colors.color-configs.settings.onenord"
-require "colors.color-configs.settings.tokyonight"
--- lua/colors/colorscheme.lua
+++ lua/colors/colorscheme.lua
@@ -1,3 +1,14 @@
+-- Put your colorscheme config files down below
+require "colors.color-configs.catppuccin"
+require "colors.color-configs.dracula"
+require "colors.color-configs.gruvbaby"
+require "colors.color-configs.kanagawa"
+require "colors.color-configs.nord"
+-- require "colors.color-configs.onedark"
+-- require "colors.color-configs.onenord"
+require "colors.color-configs.tokyonight"
+
+-- Set your colorscheme
 local colorscheme = "catppuccin"
 --[[ aurora, catppuccin, codemonkey, darkplus, dracula, ferrum, gruvbox-baby,
 kanagawa, lunar, nord, onedark, onedarker, onedarkest, onenord, spacedark,
