b0c0c5386ed65cdde6714ce2d6b75c4891d29fd2
Mateo Arcidiacono <mateoarcidiacono@gmail.com>
2022-07-12 22:08:58 -0300

    Removed old code

--- lua/plugins/settings/cmp.lua
+++ lua/plugins/settings/cmp.lua
@@ -16,34 +16,6 @@ local check_backspace = function()
 end
 
 --   פּ ﯟ   some other good icons
---[[ local kind_icons = {
-  Text = "",
-  Method = "m",
-  Function = "",
-  Constructor = "",
-  Field = "",
-  Variable = "",
-  Class = "",
-  Interface = "",
-  Module = "",
-  Property = "",
-  Unit = "",
-  Value = "",
-  Enum = "",
-  Keyword = "",
-  Snippet = "",
-  Color = "",
-  File = "",
-  Reference = "",
-  Folder = "",
-  EnumMember = "",
-  Constant = "",
-  Struct = "",
-  Event = "",
-  Operator = "",
-  TypeParameter = "",
-} ]]
-
 local kind_icons = {
 	Text = "",
 	Method = "",
