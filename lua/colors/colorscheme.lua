c4282a6ce4f6b6a76ef87c939929cdf5700b6b2d
Mateo Arcidiacono <mateoarcidiacono@gmail.com>
2022-07-15 18:19:40 -0300

    Changed colorscheme

--- lua/colors/colorscheme.lua
+++ lua/colors/colorscheme.lua
@@ -12,7 +12,7 @@ require "colors.color-configs"
 -- tokyonight
 -- zephyr
 
-local colorscheme = "darkplus"
+local colorscheme = "tokyonight"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
