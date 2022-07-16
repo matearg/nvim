7d5a2d8a5eac3e8819d730009675514bc7a4d40d
Mateo Arcidiacono <mateoarcidiacono@gmail.com>
2022-07-16 01:44:44 -0300

    Night time

--- lua/colors/color-configs/tokyonight.lua
+++ lua/colors/color-configs/tokyonight.lua
@@ -1,5 +1,5 @@
 -- Tokyonight
-vim.g.tokyonight_style = "storm"
+vim.g.tokyonight_style = "night"
 vim.g.tokyonight_terminal_colors = true
 vim.g.tokyonight_italic_comments = true
 vim.g.tokyonight_italic_keywords = false
